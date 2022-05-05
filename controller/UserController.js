ee502d30efe057ce26492cdef65af74bc5e1e9b8
Phillip2608 <luizlindo2416@gmail.com>
2022-05-05 17:01:33 -0300

    Foi adicionado o contador de usuarios e de administradores

--- controller/UserController.js
+++ controller/UserController.js
@@ -16,7 +16,7 @@ class UserController{
 
             let values = this.getValues();
 
-            values.photo = "";
+            if(!values) return false;
             
             this.getPhoto().then(
                 content =>{
@@ -111,6 +111,8 @@ class UserController{
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser); 
+
         tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -124,6 +126,28 @@ class UserController{
         `;
 
         this.tableEl.appendChild(tr);
+
+        this.updateCount();
+
+
     }
 
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr=>{
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if(user._admin) numberAdmin++;
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+
+    }
 }
